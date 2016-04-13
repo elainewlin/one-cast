fac26c5abe5df994313b2a3c5db42e496919e991
Elaine Lin <elin1@mit.edu>
2016-04-13 18:35:10 -0400

    Fixed weird highlighting problem

--- dist/js/sb-admin-2.js
+++ dist/js/sb-admin-2.js
@@ -26,11 +26,15 @@ $(function() {
         }
     });
 
-    var url = window.location;
-    var element = $('ul.nav a').filter(function() {
-        return this.href == url || url.href.indexOf(this.href) == 0;
-    }).addClass('active').parent().parent().addClass('in').parent();
-    if (element.is('li')) {
-        element.addClass('active');
-    }
+
+    $(".sidebarlink").click(function(event) {
+        $(".sidebarlink").removeClass('active');
+        $(event.target).addClass('active');
+    });
+
+    $(".dropdown-toggle").click(function(event) {
+        $(".dropdown-toggle").removeClass('active');
+        $(event.target).addClass('active');
+    });
+
 });
