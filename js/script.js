bab171dd2c3a607472326255431e238e9619901f
Elaine Lin <elin1@mit.edu>
2016-04-24 11:03:39 -0400

    Better descriptions for plays

--- js/script.js
+++ js/script.js
@@ -183,22 +183,23 @@ onecastApp.controller('actorController', function($scope, $rootScope, $window) {
 
 onecastApp.controller('searchController', function($scope, ngTableParams) {
     $scope.primary = 'purple';
-    var data = [{title: "Romeo & Juliet", date: "2016-04-29", location: "Chesapeake, VA", description: "les miserables"},{title: "Les Miserables", date: "2016-04-29", location: "Chesapeake, VA", description: "les miserables"},
-                {title: "Cabaret", date: "2016-05-09", location: "Newark, NJ", description: "cabaret"},
-                {title: "Mamma Mia", date: "2016-05-17", location: "Detroit, MI", description: "mamma mia"},
-                {title: "Grease", date: "2016-05-26", location: "Dallas, TX", description: "grease"},
-                {title: "Into the Woods", date: "2016-06-06", location: "Honolulu, HI", description: "into the woods"},
-                {title: "Cinderella", date: "2016-07-11", location: "Albuquerque, NM", description: "cinderella"},
-                {title: "The Phantom of the Opera", date: "2016-07-12", location: "Jersey City, NJ", description: "phantom of the opera"},
-                {title: "The Wizard of Oz", date: "2016-07-19", location: "Sacramento, CA", description: "wizard of oz"},
-                {title: "Titanic", date: "2016-07-25", location: "Pittsburgh, PA", description: "leonardo dicaprio <3"},
-                {title: "A Streetcar Named Desire", date: "2016-08-18", location: "Fort Worth, TX", description: "street car named desire"},
-                {title: "Breakfast at Tiffany's", date: "2016-08-30", location: "Aurora, CO", description: "breakfast at tiffanys"},
-                {title: "Singin' in the Rain", date: "2016-09-15", location: "Oklahoma City, OK", description: "singin in the rain"},
-                {title: "Hairspray", date: "2016-09-27", location: "Virginia Beach, VA", description: "hairspray"},
-                {title: "Hamlet", date: "2016-10-03", location: "Toledo, OH", description: "hamlet"},
-                {title: "The Duchess of Malfi", date: "2016-10-10", location: "Memphis, TN", description: "duchess of malfi"},
-                {title: "Uncle Vanya", date: "2016-11-29", location: "Tampa, FL", description: "uncle vanya"}];
+    var data = [{title: "Romeo & Juliet", date: "2016-04-29", location: "Chesapeake, VA", description: "Romeo and Juliet is a tragedy written by William Shakespeare early in his career about two young star-crossed lovers whose deaths ultimately reconcile their feuding families."},
+                {title: "Les Miserables", date: "2016-04-29", location: "Chesapeake, VA", description: "Les Misérables is a French historical novel by Victor Hugo, first published in 1862, that is considered one of the greatest novels of the 19th century."},
+                {title: "Cabaret", date: "2016-05-09", location: "Newark, NJ", description: "Cabaret is a musical based on a book written by Christopher Isherwood, music by John Kander and lyrics by Fred Ebb. "},
+                {title: "Mamma Mia", date: "2016-05-17", location: "Detroit, MI", description: "Mamma Mia! is a jukebox musical written by British playwright Catherine Johnson, based on the songs of ABBA."},
+                {title: "Grease", date: "2016-05-26", location: "Dallas, TX", description: "Grease is a 1971 musical by Jim Jacobs and Warren Casey with additional songs written by John Farrar."},
+                {title: "Into the Woods", date: "2016-06-06", location: "Honolulu, HI", description: "Into the Woods is a musical with music and lyrics by Stephen Sondheim and book by James Lapine."},
+                {title: "Cinderella", date: "2016-07-11", location: "Albuquerque, NM", description: "Cinderella, or The Little Glass Slipper is a folk tale embodying a myth-element of unjust oppression/triumphant reward."},
+                {title: "The Phantom of the Opera", date: "2016-07-12", location: "Jersey City, NJ", description: "The Phantom of the Opera is a musical with music by Andrew Lloyd Webber and lyrics by Charles Hart with additions from Richard Stilgoe. Lloyd Webber and Stilgoe also wrote the musical's book together."},
+                {title: "The Wizard of Oz", date: "2016-07-19", location: "Sacramento, CA", description: "Dorothy Gale is swept away to a magical land in a tornado and embarks on a quest to see the Wizard who can help her return home."},
+                {title: "Titanic", date: "2016-07-25", location: "Pittsburgh, PA", description: "RMS Titanic was a British passenger liner that sank in the North Atlantic Ocean in the early morning of 15 April 1912, after colliding with an iceberg during her maiden voyage from Southampton, UK, to New York City, US."},
+                {title: "A Streetcar Named Desire", date: "2016-08-18", location: "Fort Worth, TX", description: "A Streetcar Named Desire is a 1947 play written by American playwright Tennessee Williams which received the Pulitzer Prize for Drama in 1948."},
+                {title: "Breakfast at Tiffany's", date: "2016-08-30", location: "Aurora, CO", description: "Based on Truman Capote's novel, this is the story of a young woman in New York City who meets a young man when he moves into her apartment building. He is with an older woman who is very wealthy, but he wants to be a writer. "},
+                {title: "Singin' in the Rain", date: "2016-09-15", location: "Oklahoma City, OK", description: "A spoof of the turmoil that afflicted the movie industry in the late 1920s when movies went from silent to sound. When two silent movie stars', Don Lockwood and Lina Lamont, latest movie is made into a musical a chorus girl is brought in to dub Lina's speaking and singing. "},
+                {title: "Hairspray", date: "2016-09-27", location: "Virginia Beach, VA", description: "In 1960s Baltimore, dance-loving teen Tracy Turnblad (Nikki Blonsky) auditions for a spot on The Corny Collins Show and wins. She becomes an overnight celebrity, a trendsetter in dance, fun and fashion. "},
+                {title: "Hamlet", date: "2016-10-03", location: "Toledo, OH", description: "The Tragedy of Hamlet, Prince of Denmark, often shortened to Hamlet, is a tragedy written by William Shakespeare at an uncertain date between 1599 and 1602. "},
+                {title: "The Duchess of Malfi", date: "2016-10-10", location: "Memphis, TN", description: "The Duchess of Malfi is a macabre, tragic play written by the English dramatist John Webster in 1612–13. It was first performed privately at the Blackfriars Theatre, then before a more general audience at The Globe, in 1613–14. "},
+                {title: "Uncle Vanya", date: "2016-11-29", location: "Tampa, FL", description: "Uncle Vanya is a play by the Russian playwright Anton Chekhov. It was first published in 1897 and received its Moscow première in 1899 in a production by the Moscow Art Theatre, under the direction of Konstantin Stanislavski."}];
     
     $scope.isCollapsed=true;
     
