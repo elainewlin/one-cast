d424741a2671415db5ea75abb9b717890a24fa5b
Aaron Vontell <vontell@mit.edu>
2016-05-02 17:17:34 -0400

    Added form validation to account creation, fixes #29

--- js/account.js
+++ js/account.js
@@ -2,8 +2,9 @@ var accountApp = angular.module('accountApp', ['ngMaterial', 'ngMessages']);
 
 accountApp.controller('formController', function($scope, $mdDialog) {
     $scope.nameError = "Valid name required";
-    $scope.eyeColors = ["Blue", "Brown", "Hazel", "Other"];
-    $scope.hairColors = ["Brunette", "Blonde", "Red", "White", "Other"];
+    $scope.eyeColors = ["Blue", "Brown", "Hazel", "Grey", "Green", "Other"];
+    $scope.hairColors = ["Black", "Blonde", "Brown", "Red", "Grey", "White", "Auburn", "Salt and Pepper", "Other"];
+    $scope.builds = ["Slim", "Medium", "Muscular", "Large", "Very Large"];
     //console.log($ngMessages);
     
     $scope.datePicker = {date: {startDate: "", endDate: ""}};
