f363e4a59d555002fef621fe896027bcafa89e55
Aizhan <toregozh@gmail.com>
2016-05-03 08:06:10 -0400

    fix css at actors account creation dropdown menus; add material datepicker close #69

--- js/account.js
+++ js/account.js
@@ -7,31 +7,6 @@ accountApp.controller('formController', function($scope, $mdDialog) {
     $scope.builds = ["Slim", "Medium", "Muscular", "Large", "Very Large"];
     //console.log($ngMessages);
     
-    $scope.datePicker = {date: {startDate: "", endDate: ""}};
-    
-    $scope.byDateRange = function (fieldName, minValue, maxValue) {
-        
-        // reformat the dates
-        if(typeof(minValue)!= "string" && minValue != null) {
-            minValue = minValue.format("YYYY-MM-DD");
-
-        }
-        if(typeof(maxValue)!= "string" && maxValue != null) {
-            maxValue = maxValue.format("YYYY-MM-DD");
-        }        
-        if(minValue != "" && minValue != null) {
-            if(maxValue != "" && maxValue != null) {
-                if (minValue === undefined) minValue = Number.MIN_VALUE;
-                if (maxValue === undefined) maxValue = Number.MAX_VALUE;
-
-                return function predicateFunc(item) {
-                    return minValue <= item[fieldName] && item[fieldName] <= maxValue;
-                };
-            }
-        }
-        
-        
-    };
     
 });
 
